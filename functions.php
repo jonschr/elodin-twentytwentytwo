0e2e2eaf8279eec2aa17f672f55a0f2d03efafac
Jon Schroeder <jon@elod.in>
2022-08-30 16:48:04 -0500

    Lots of little stuff

--- functions.php
+++ functions.php
@@ -14,3 +14,14 @@ define( 'CHILD_THEME_DIR', dirname( __FILE__ ) );
 foreach ( glob( CHILD_THEME_DIR . "/inc/*/*.php", GLOB_NOSORT ) as $filename ){
     require_once $filename;
 }
+
+// add an image size
+add_image_size( 'square', 500, 500, true );
+
+// add the image size to galleries
+add_filter( 'image_size_names_choose', 'ettt_add_gallery_sizes' );
+function ettt_add_gallery_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'square' => __( 'Square' ),
+    ) );
+}

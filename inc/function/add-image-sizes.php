7555c1be429f28230c413b291a8f3a296ec58d1e
Jon Schroeder <jon@elod.in>
2022-11-10 11:18:45 -0600

    Minor

--- inc/function/add-image-sizes.php
+++ inc/function/add-image-sizes.php
@@ -2,6 +2,7 @@
 
 // add an image size
 add_image_size( 'square', 500, 500, true );
+add_image_size( 'blog', 820, 300, true );
 
 // add the image size to galleries
 add_filter( 'image_size_names_choose', 'ettt_add_gallery_sizes' );

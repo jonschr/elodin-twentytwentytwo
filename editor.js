ab837967f587379f55065f0c84a76773563726c3
Jon Schroeder <jon@elod.in>
2023-03-11 20:09:52 -0600

    Several small changes

--- editor.js
+++ editor.js
@@ -4,16 +4,16 @@ wp.domReady(() => {
     // wp.blocks.unregisterBlockType('core/audio');
     // wp.blocks.unregisterBlockType('core/button');
     // wp.blocks.unregisterBlockType('core/buttons');
-    wp.blocks.unregisterBlockType('core/calendar');
+    // wp.blocks.unregisterBlockType('core/calendar');
     // wp.blocks.unregisterBlockType('core/categories');
     // wp.blocks.unregisterBlockType('core/classic');
     // wp.blocks.unregisterBlockType('core/code');
     // wp.blocks.unregisterBlockType('core/column');
     // wp.blocks.unregisterBlockType('core/columns');
-    wp.blocks.unregisterBlockType('core/cover');
+    // wp.blocks.unregisterBlockType('core/cover');
     // wp.blocks.unregisterBlockType('core/file');
-    wp.blocks.unregisterBlockType('core/latest-comments');
-    wp.blocks.unregisterBlockType('core/latest-posts');
+    // wp.blocks.unregisterBlockType('core/latest-comments');
+    // wp.blocks.unregisterBlockType('core/latest-posts');
     // wp.blocks.unregisterBlockType('core/legacy-widget'); // error thrown when unregistered
     // wp.blocks.unregisterBlockType('core/gallery');
     // wp.blocks.unregisterBlockType('core/group');
@@ -22,15 +22,15 @@ wp.domReady(() => {
     // wp.blocks.unregisterBlockType('core/image');
     // wp.blocks.unregisterBlockType('core/list');
     // wp.blocks.unregisterBlockType('core/media-text');
-    wp.blocks.unregisterBlockType('core/more');
+    // wp.blocks.unregisterBlockType('core/more');
     // wp.blocks.unregisterBlockType('core/navigation'); // error thrown when unregistered
     // wp.blocks.unregisterBlockType('core/navigation-link'); // error thrown when unregistered
-    wp.blocks.unregisterBlockType('core/nextpage');
+    // wp.blocks.unregisterBlockType('core/nextpage');
     // wp.blocks.unregisterBlockType('core/paragraph');
-    wp.blocks.unregisterBlockType('core/preformatted');
+    // wp.blocks.unregisterBlockType('core/preformatted');
     // wp.blocks.unregisterBlockType('core/pullquote');
     // wp.blocks.unregisterBlockType('core/quote');
-    wp.blocks.unregisterBlockType('core/rss');
+    // wp.blocks.unregisterBlockType('core/rss');
     // wp.blocks.unregisterBlockType('core/search');
     // wp.blocks.unregisterBlockType('core/separator');
     // wp.blocks.unregisterBlockType('core/shortcode');
@@ -39,7 +39,7 @@ wp.domReady(() => {
     // wp.blocks.unregisterBlockType('core/spacer');
     // wp.blocks.unregisterBlockType('core/subhead');
     // wp.blocks.unregisterBlockType('core/table');
-    wp.blocks.unregisterBlockType('core/tag-cloud');
+    // wp.blocks.unregisterBlockType('core/tag-cloud');
     // wp.blocks.unregisterBlockType('core/text-columns');
     // wp.blocks.unregisterBlockType('core/verse');
     // wp.blocks.unregisterBlockType('core/video');
--- functions.php
+++ functions.php
@@ -7,10 +7,13 @@
  */
 
 
-define( 'CHILD_THEME_VERSION', '0.1.7' );
+define( 'CHILD_THEME_VERSION', '0.2' );
 define( 'CHILD_THEME_DIR', dirname( __FILE__ ) );
 
 //* Include everything in /lib
 foreach ( glob( CHILD_THEME_DIR . "/inc/*/*.php", GLOB_NOSORT ) as $filename ){
     require_once $filename;
 }
+
+// disable gutenberg widgets
+// add_filter( 'use_widgets_block_editor', '__return_false' );

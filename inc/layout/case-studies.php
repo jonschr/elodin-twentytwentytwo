9dd9af283daa0d4efdb7fab2b49eadd548e82541
Jon Schroeder <jon@elod.in>
2023-11-08 10:07:41 -0600

    Minor

---
+++ inc/layout/case-studies.php
@@ -0,0 +1,46 @@
+<?php
+
+//* Output casestudies before
+// add_action( 'before_loop_layout_casestudies', 'elodin_casestudies_before' );
+function elodin_casestudies_before( $args ) {
+	// wp_enqueue_script( 'SCRIPTHANDLE' );
+}
+
+//* Output each casestudies
+add_action( 'add_loop_layout_casestudies', 'elodin_casestudies_each' );
+function elodin_casestudies_each() {
+
+	//* Global vars
+	global $post;
+	$id = get_the_ID();
+
+	//* Vars
+	$title = get_the_title();
+	$permalink = get_the_permalink();
+	$content = get_the_content();
+	$excerpt = apply_filters( 'the_content', apply_filters( 'the_content', get_the_excerpt() ) );
+	$background = get_the_post_thumbnail_url( get_the_ID(), 'large' );
+	
+	// $thing = get_post_meta( $id, 'thing', true );
+	
+	if ( !$content )
+		$permalink = null;
+	
+	//* Markup
+	if ( $permalink )
+		printf( '<a class="overlay" href="%s"></a>', $permalink );
+			
+	if ( $background )
+		printf( '<div class="featured-image" style="background-image:url( %s )"></div>', $background );
+    
+    echo '<div class="the-content">';
+    
+        echo '<div class="content-inner">';
+    
+            if ( $title )
+                printf( '<h3>%s</h3>', $title );
+        
+        echo '</div>';
+    
+    echo '</div>';
+}

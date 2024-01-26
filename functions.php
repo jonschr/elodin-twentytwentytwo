3b2d66b2a8d81ff996260a2cd9359ac3e26e79ff
Jon Schroeder <jon@elod.in>
2024-01-25 22:18:39 -0600

    Minor

--- functions.php
+++ functions.php
@@ -17,4 +17,30 @@ foreach ( glob( CHILD_THEME_DIR . "/inc/*/*.php", GLOB_NOSORT ) as $filename ){
 
 add_filter( 'media_library_infinite_scrolling', '__return_true' );
 
-
+function rf_add_property_title_to_floorplan_title( $title ) {
+		
+	$property_id = get_post_meta( get_the_ID(), 'property_id', true );
+			
+	// do a query for properties with this property_id
+	$args = array(
+		'post_type' => 'properties',
+		'posts_per_page' => 1,
+		'meta_query' => array(
+			array(
+				'key' => 'property_id',
+				'value' => $property_id,
+			)
+		)
+	);
+	
+	$propertylist = get_posts( $args );
+	$property = $propertylist[0];
+	
+	// var_dump( $property );
+	
+	if ( $property->post_title )
+		$title = sprintf( '%s - %s', $property->post_title, $title );
+		
+	return $title;
+}
+add_filter( 'rentfetch_filter_floorplan_title', 'rf_add_property_title_to_floorplan_title', 10, 1 );

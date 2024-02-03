eb923c7c055453e01b9f6612b6b50ab35aea0f42
Jon Schroeder <jon@elod.in>
2024-02-02 19:50:33 -0600

    Minior

--- editor.js
+++ editor.js
@@ -2,53 +2,57 @@
 // Use this in console to see list of blocks that haven't been unregistered
 
 wp.domReady(() => {
-    // wp.blocks.unregisterBlockType('core/rss');
-    // wp.blocks.unregisterBlockType('core/tag-cloud');
-    // wp.blocks.unregisterBlockType('core/text-columns');
-    // wp.blocks.unregisterBlockType('core/verse');
-    // wp.blocks.unregisterBlockType('core/columns');
-    // wp.blocks.unregisterBlockType('core/column');
-    // wp.blocks.unregisterBlockType('core/cover');
-    // wp.blocks.unregisterBlockType('core/media-text');
-    // wp.blocks.unregisterBlockType('generateblocks/button-container');
-    // wp.blocks.unregisterBlockType('generateblocks/button');
+	// wp.blocks.unregisterBlockType('core/rss');
+	// wp.blocks.unregisterBlockType('core/tag-cloud');
+	// wp.blocks.unregisterBlockType('core/text-columns');
+	// wp.blocks.unregisterBlockType('core/verse');
+	// wp.blocks.unregisterBlockType('core/columns');
+	// wp.blocks.unregisterBlockType('core/column');
+	// wp.blocks.unregisterBlockType('core/cover');
+	// wp.blocks.unregisterBlockType('core/media-text');
+	// wp.blocks.unregisterBlockType('generateblocks/button-container');
+	// wp.blocks.unregisterBlockType('generateblocks/button');
 });
 
 //* Add alt styles
 wp.domReady(() => {
-    wp.blocks.registerBlockStyle('core/paragraph', [
-        {
-            name: 'default',
-            label: 'Default',
-            isDefault: true,
-        },
-        {
-            name: 'kicker',
-            label: 'Kicker',
-        },
-    ]);
+	wp.blocks.registerBlockStyle('core/paragraph', [
+		{
+			name: 'default',
+			label: 'Default',
+			isDefault: true,
+		},
+		{
+			name: 'kicker',
+			label: 'Kicker',
+		},
+	]);
 
-    wp.blocks.registerBlockStyle('core/list', [
-        {
-            name: 'default',
-            label: 'Default',
-            isDefault: true,
-        },
-        {
-            name: 'checkmark',
-            label: 'Checkmark',
-        },
-    ]);
+	wp.blocks.registerBlockStyle('core/list', [
+		{
+			name: 'default',
+			label: 'Default',
+			isDefault: true,
+		},
+		{
+			name: 'checkmark',
+			label: 'Checkmark',
+		},
+		{
+			name: 'caret',
+			label: 'Caret',
+		},
+	]);
 
-    // wp.blocks.registerBlockStyle('core/heading', [
-    //     {
-    //         name: 'default',
-    //         label: 'Default',
-    //         isDefault: true,
-    //     },
-    //     {
-    //         name: 'red-line',
-    //         label: 'Red line',
-    //     },
-    // ]);
+	// wp.blocks.registerBlockStyle('core/heading', [
+	//     {
+	//         name: 'default',
+	//         label: 'Default',
+	//         isDefault: true,
+	//     },
+	//     {
+	//         name: 'red-line',
+	//         label: 'Red line',
+	//     },
+	// ]);
 });

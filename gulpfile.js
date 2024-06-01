4870c0f2fde1c6bdc1c2365e91c6bfe91285f2d5
Jon Schroeder <jon@elod.in>
2024-06-01 01:37:08 -0500

    Minor

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,53 @@ var sassGlob = require('gulp-sass-glob');
 const cleanCSS = require('gulp-clean-css');
 
 gulp.task('style-dev', function () {
-    return gulp
-        .src('assets/css/theme-style.scss')
-        .pipe(sassGlob())
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('assets/dist/'));
+	return gulp
+		.src('assets/css/theme-style.scss')
+		.pipe(sassGlob())
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('assets/dist/'));
 });
 
 gulp.task('style-prod', function () {
-    return gulp
-        .src('assets/css/theme-style.scss')
-        .pipe(sassGlob())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('assets/dist/'));
+	return gulp
+		.src('assets/css/theme-style.scss')
+		.pipe(sassGlob())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest('assets/dist/'));
 });
 
 // don't do a sourcemap for gutenberg
 gulp.task('gutenberg-style', function () {
-    return gulp
-        .src('assets/css/gutenberg-style.scss')
-        .pipe(sassGlob())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('assets/dist/'));
+	return gulp
+		.src('assets/css/gutenberg-style.scss')
+		.pipe(sassGlob())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest('assets/dist/'));
+});
+
+// don't do a sourcemap for wp receipe maker
+gulp.task('bean', function () {
+	return gulp
+		.src('wprm-templates/recipe/bean/bean.scss')
+		.pipe(sassGlob())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest('wprm-templates/recipe/bean/'));
 });
 
 // don't do a sourcemap for editor
 gulp.task('editor-style', function () {
-    return gulp
-        .src('assets/css/editor-style.scss')
-        .pipe(sassGlob())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('assets/dist/'));
+	return gulp
+		.src('assets/css/editor-style.scss')
+		.pipe(sassGlob())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(gulp.dest('assets/dist/'));
 });
 
 //! JAVASCRIPT
@@ -55,32 +66,33 @@ var jsFiles = 'assets/js/**/*.js';
 var jsDest = 'assets/dist/';
 
 gulp.task('scripts', function () {
-    return gulp
-        .src(jsFiles)
-        .pipe(
-            rename(function (path) {
-                path.basename += '.min';
-            })
-        )
-        .pipe(uglify())
-        .pipe(gulp.dest(jsDest));
+	return gulp
+		.src(jsFiles)
+		.pipe(
+			rename(function (path) {
+				path.basename += '.min';
+			})
+		)
+		.pipe(uglify())
+		.pipe(gulp.dest(jsDest));
 });
 
 //* Watchers
 gulp.task('dev', function () {
-    gulp.watch(
-        'assets/css/**/*.scss',
-        gulp.series(['style-dev', 'gutenberg-style', 'editor-style'])
-    );
-    gulp.watch('assets/js/**/*.js', gulp.series(['scripts']));
+	gulp.watch(
+		'assets/css/**/*.scss',
+		gulp.series(['style-dev', 'gutenberg-style', 'editor-style'])
+	);
+	gulp.watch('assets/js/**/*.js', gulp.series(['scripts']));
+	gulp.watch('wprm-templates/recipe/**/*.scss', gulp.series(['bean']));
 });
 
 gulp.task('prod', function () {
-    gulp.watch(
-        'assets/css/**/*.scss',
-        gulp.series(['style-prod', 'gutenberg-style', 'editor-style'])
-    );
-    gulp.watch('assets/js/**/*.js', gulp.series(['scripts']));
+	gulp.watch(
+		'assets/css/**/*.scss',
+		gulp.series(['style-prod', 'gutenberg-style', 'editor-style'])
+	);
+	gulp.watch('assets/js/**/*.js', gulp.series(['scripts']));
 });
 
 //* Set the default task to dev

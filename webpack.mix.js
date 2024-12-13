8506d7bc692301d7d65f284e62d1bd986cca6a3d
Jon Schroeder <jon@elod.in>
2024-12-13 17:04:36 -0600

    Fixing watching command

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,19 @@
 const mix = require('laravel-mix');
 const globImporter = require('node-sass-glob-importer');
 
-// Explicitly set Webpack's devtool option for source maps
-mix.webpackConfig({
-	devtool: 'source-map',
-});
-
+// Compile SCSS files
 mix.sass('assets/css/theme-style.scss', 'assets/dist', {
 	sassOptions: {
 		importer: globImporter(),
 	},
-});
-
-mix.sass('assets/css/gutenberg-style.scss', 'assets/dist', {
-	sassOptions: {
-		importer: globImporter(),
-	},
-});
-
-mix.sass('assets/css/editor-style.scss', 'assets/dist', {
-	sassOptions: {
-		importer: globImporter(),
-	},
-});
+})
+	.sass('assets/css/gutenberg-style.scss', 'assets/dist', {
+		sassOptions: {
+			importer: globImporter(),
+		},
+	})
+	.sass('assets/css/editor-style.scss', 'assets/dist', {
+		sassOptions: {
+			importer: globImporter(),
+		},
+	});

cbd99db0691b89c347bd92fa7889d0a24cb29b40
Jon Schroeder <jon@elod.in>
2024-12-13 16:51:53 -0600

    Some fixes for updated version of compiler

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,11 @@
 const mix = require('laravel-mix');
 const globImporter = require('node-sass-glob-importer');
 
+// Explicitly set Webpack's devtool option for source maps
+mix.webpackConfig({
+	devtool: 'source-map',
+});
+
 mix.sass('assets/css/theme-style.scss', 'assets/dist', {
 	sassOptions: {
 		importer: globImporter(),
